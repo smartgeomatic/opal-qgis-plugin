5ff4403566ea74b9685fb1112391894a20a73a52
Wojtek Oroński <w.oronski@acrux.net.pl>
2018-07-20 22:33:26 +0200

    encrypts api key

--- nmaps/apikey.py
+++ nmaps/apikey.py
@@ -1,9 +1,6 @@
 import os
-import base64
-import hashlib
+import random
 from uuid import getnode as get_mac
-from Crypto import Random
-from Crypto.Cipher import AES
 
 class ApiKey:
 
@@ -12,33 +9,37 @@ class ApiKey:
     def __init__(self):
         self._apikey_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..', self.file_name)
         self._apikey = None
-        self.bs = 32
-        self.key = hashlib.sha256(str(get_mac())).digest()
+        self.key = "abcdefghijklmnopqrstuvwxyz012356789"
+        self.n = 5
 
     def apikeyFile(self):
         return self._apikey_file
 
-    def encrypt(self, raw):
-        raw = self._pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw))
+    def encrypt(self, plaintext):
+        result = ''
+        for l in plaintext:
+            try:
+                i = (self.key.index(l) + self.n) % len(self.key)
+                result += self.key[i]
+            except ValueError:
+                result += l
 
-    def decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        return result
 
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+    def decrypt(self, ciphertext):
+        result = ''
+        for l in ciphertext:
+            try:
+                i = (self.key.index(l) - self.n) % len(self.key)
+                result += self.key[i]
+            except ValueError:
+                result += l
 
-    @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s) - 1:])]
+        return result
 
     def set(self, apikey):
         apikey_file = open(self._apikey_file, 'w')
+        print self.encrypt(apikey.strip())
         apikey_file.write(self.encrypt(apikey.strip()))
         apikey_file.close()
 
